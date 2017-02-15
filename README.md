ede9c2179d4400e5e8478fb2289d54e3bf05c97d
MedZed <Medy484@gmail.com>
2017-02-15 18:11:38 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Quizzer
 Save quiz questions with answers in Json file (node)
+
+## Exemple : 
+```json
+[
+    {
+        "question":"What's the tallest building in the world",
+        "correct":"Burj khalifa",
+        "falseOne":"Burj al arab",
+        "falseTwo":"Ifel toure",
+        "falseThree":"The great wall of China"
+    }
+]
+```
+
+## Screenshot :
+
+![Quizzer](./Screenshot.JPG)
---
+++ src/app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('views', 'src/views');
+app.set('view engine', 'jade');
+
+app.use(express.static('public')); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function (request, response) {
+	response.render("add");
+
+});
+
+app.get('/')
+
+app.post('/newquiz', function (request, response) {
+
+	var question = request.body.question;
+	var correct = request.body.correct;
+	var falseOne = request.body.falseOne;
+	var falseTwo = request.body.falseTwo;
+	var falseThree = request.body.falseThree;
+
+
+	var newQuiz = {}
+	newQuiz.question = question
+	newQuiz.correct = correct
+	newQuiz.falseOne = falseOne
+	newQuiz.falseTwo = falseTwo
+	newQuiz.falseThree = falseThree
+	fs.readFile("./resources/quiz.json", function (err, data) {
+
+		var parsedData = JSON.parse(data);
+
+		parsedData.push(newQuiz);
+
+		writeFileFunction(parsedData);
+
+	});
+
+	function writeFileFunction(newQuizInfo) {
+
+		var newQuizInfo = JSON.stringify(newQuizInfo);
+
+		fs.writeFile("./resources/quiz.json", newQuizInfo, function (err) {
+			
+
+			if (err) {
+				throw err;
+			}
+		});
+	};
+				// response.render('add', { message: "seccessfuly added a new question" , msgType: "alert alert-dismissible alert-info" });
+
+	response.redirect('/');
+
+});
+
+
+var server = app.listen(3000, function () {
+	console.log('Example app listening on port: ' + server.address().port);
+});
